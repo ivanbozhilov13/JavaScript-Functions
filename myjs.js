d6496ef7cccbfa04c9a3210af4aa2b57311c5a72
Ivan13 <ivanbozhilov13@gmx.ch>
2020-09-07 23:45:40 +0200

    myjs.js

--- myjs.js
+++ myjs.js
@@ -1,7 +1,6 @@
 /*Function are foundamental in JavaScript. Their usage is highly spread in all 
 environments of the language. Is vital to master them and learn how to use them
 properly*/
-
 /*This example we have a functions that prints 'Hello World!' inside the doc.
 Function's name is greeting and inside the brackets we have its parameters.
 the with the DOM doc. we access the doc's ID and print on screen 'Hello World'*/
@@ -10,7 +9,25 @@ function greeting(greet){
                   executed by the compiler! We can have several different 'return' commands 
                   though.*/  
 }
-//Alert('Hello World!') is one way to print out the functions when calling her
+//alert('Hello World!') is one way to print out the functions when calling her
 console.log('Hello World'); //Another way to execute the command
 document.write('Hello World'); //Another way to execute the command
-var x = document.getElementById('greeting').innerHTML = 'Hello World!'; //Executing command
+let x = document.getElementById('greeting').innerHTML = 'Hello World!'; //Executing command
+
+
+//Function with 2 numbers. Parameters declared ( )
+function getArea(width, height){ //This a function declarations
+    let area = width * height; //Instruction what we want the fucntion to do
+    return area; //returning the result
+};
+var el = document.getElementById('area'); //Seelecting within which element to call the funct.
+el.textContent = getArea(5, 5);
+
+
+//This is a function expression
+let area = function (width1, height1){ //Function declared as anonymous. Names is omitted
+    return width1 * height1; //Function expression cant be called earlier before the 
+                            //the interpreter gets to her
+};
+let size = area(6, 6);
+let x1 = document.getElementById('area1').innerHTML = size;    
