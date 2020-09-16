f8f0438843e64ec5bec2ebe02f2007bf938f077b
Ivan13 <ivanbozhilov13@gmx.ch>
2020-09-16 20:02:36 +0200

    edit

--- myjs.js
+++ myjs.js
@@ -242,3 +242,17 @@ for (let i = 0; i < arr.length; i++){
     total += arr[i];
 }
 console.log(total);
+
+//For loop in a function to multiply all the values of an array
+function multiplyAll(arr){
+    let product = 1;
+    for (let i = 0; i < arr.length; i++){
+        for (let j = 0; j < arr[i].length; j++){
+            product *= arr[i][j];
+        }
+    }
+    return product
+}
+console.log(multiplyAll([[1,2],[3,4],[5,6,7]]));
+
+//
