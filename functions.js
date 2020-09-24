92ede348cb453318efc190e67e11fb8399f8a115
Ivan13 <ivanbozhilov13@gmx.ch>
2020-09-24 13:57:36 +0200

    functions edit

---
+++ functions.js
@@ -0,0 +1,5 @@
+//Functions in JS. Both ES5 and ES6
+function hello(){
+    console.log('Hello World!'); //The output we expect and want to the console
+}
+hello(); //We are calling the function and loggin to the console
