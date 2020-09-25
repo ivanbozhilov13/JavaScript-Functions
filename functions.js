c0924c1a4124de3c31e9abd3342971164dc20727
Ivan13 <ivanbozhilov13@gmx.ch>
2020-09-25 22:26:51 +0200

    global editing

--- functions.js
+++ functions.js
@@ -3,3 +3,61 @@ function hello(){
     console.log('Hello World!'); //The output we expect and want to the console
 }
 hello(); //We are calling the function and loggin to the console
+
+//Returning the first index and the last one of the array
+function example(firstOne, lastOne){
+    let arr = [1,2,3,80,-500];
+    firstOne = arr[0];
+    lastOne = arr[arr.length-1];
+    return firstOne;
+}
+console.log(example());
+
+//
+function equalityCheck(a, b){
+    let equality = a === b;
+    return equality;
+}
+console.log(equalityCheck(1, true)); //Output: true
+console.log(equalityCheck(0, "0")); //Output: false. Second 0 is a String
+console.log(equalityCheck(1, 1)); //Output: false  
+
+//Same function as above but done with if...else statement
+function equalityCheck2(a, b){
+    if(typeof(a) === typeof(b)){
+        return true;
+    }  else if (typeof(a) !== typeof(b)){
+        return false;
+    }
+}
+console.log(equalityCheck(1, true)); //Output: true
+console.log(equalityCheck(0, "0")); //Output: false. Second 0 is a String
+console.log(equalityCheck(1, 1));
+
+//
+function profitableGamble(prob, size, pay){
+    if(prob * size > pay){
+        return true;
+    } else {
+        return false;
+    }
+}
+console.log(profitableGamble(0.2, 50, 9));
+console.log(profitableGamble(0.9, 1, 2));
+console.log(profitableGamble(0.9, 3, 2));
+
+//
+function isSameNum(num1, num2){
+    if(num1 === num2){                  /*Same but shortert. just Add return ===...:
+                                        return num1 === num2;
+                                        This way if it's not true then logically is false.*/
+        return true;
+    }   else {
+        return false;
+    }
+}
+console.log(isSameNum(10, 10));
+console.log(isSameNum(10, '10'));
+
+//
+
