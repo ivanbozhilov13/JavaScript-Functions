04977a4ae8935206d2ce4db6f66283902dff8d05
Ivan13 <ivanbozhilov13@gmx.ch>
2020-10-06 23:44:03 +0200

    edit functions

--- functions.js
+++ functions.js
@@ -101,7 +101,15 @@ function concatName(firstName, lastName){
   }
 console.log(concatName("First", "Last"));
 
-
+//
+function getSize(width, height, depth){
+    var area = width * height;
+    var volume = width * height * depth;
+    var sizes = [area, volume];
+    return sizes;
+}
+console.log(getSize(3,2,3)[0]);
+console.log(getSize(3,2,3)[1]);
 
 
   
